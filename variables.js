466bbf74412c9fa2d30aec1873425895e71a5b56
ahmednaveedsumra <ahmednaveedsumra@gmail.com>
2024-10-09 19:34:41 +0500

    Variables commit

---
+++ variables.js
@@ -0,0 +1,45 @@
+//const
+
+const var1=1;
+console.log(var1);
+
+{
+    const var1 =2;
+    console.log(var1);
+    
+}
+
+console.log(var1);
+
+
+//let
+
+let var2=1;
+console.log(var2);
+
+{
+    let var2 =2;
+    console.log(var2);
+    
+}
+
+console.log(var2);
+
+
+//var
+
+var var3=1;
+console.log(var3);
+
+{
+    var var3 =2;
+    console.log(var3);
+    
+}
+
+console.log(var3);
+
+
+
+
+
